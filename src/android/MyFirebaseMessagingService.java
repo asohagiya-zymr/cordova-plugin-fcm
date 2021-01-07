238965173dbdc6a96b4804fed1b4669f10eabc11
sharmeen-mozzaz <77104262+sharmeen-mozzaz@users.noreply.github.com>
2021-01-07 06:33:56 -0500

    Update MyFirebaseMessagingService.java
    Needed to update the bundle id for condition advisor which is hardcoded

--- src/android/MyFirebaseMessagingService.java
+++ src/android/MyFirebaseMessagingService.java
@@ -23,7 +23,7 @@ import com.google.firebase.messaging.RemoteMessage;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.mozzaz.lifetiles.MainActivity;
+import com.mozzaz.conditionadvisor.MainActivity;
 /**
  * Created by Felipe Echanique on 08/06/2016.
  */
